61c459031ec84b4292a0b7e3b9675dd92a1b066a
Xzud <shijirox44@gmail.com>
2026-08-22 17:30:13 +0800

    Add employee position seeding command; implement logic to assign positions to employees

---
+++ apps/positions/management/commands/seed_positions.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from faker import Faker
+from apps.employees.models import Employee
+from django.contrib.auth import get_user_model
+
+from apps.positions.models import EmployeePosition
+
+
+class Command(BaseCommand):
+    help = "Seed employees with positions"
+
+    def handle(self, *args, **options):
+        employees = Employee.objects.all()
+
+        positions = [
+            {"name": "IT Administrator", "level": 999},
+            {"name": "Staff", "level": 10},
+            {"name": "Manager", "level": 100},
+        ]
+
+        for position in positions:
+            position["object"] = EmployeePosition.objects.get_or_create(
+                name=position["name"], level=position["level"]
+            )
+
+        print(f"positions: {positions}")
+
+        for i, employee in enumerate(employees):
+            if not employee.position:
+                position, _ = positions[i%3]["object"]
+                employee.position = position
+                employee.save()
+
+        
